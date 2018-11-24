cf11715dea8a95b7533560f73480df7f43a10fc3
JaeHyeon-Yu <43131666+JaeHyeon-Yu@users.noreply.github.com>
2018-11-24 16:15:58 +0900

    몬스터 애니메이션 함수
    다음 행동을 랜덤으로 정해주는 함수 만들었음.
    끝까지는 이동 못하도록 좌에서 2번째 칸에선 이동이 불가능함.

--- Project/monster.py
+++ Project/monster.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import main_state
+import random
 
 IDLE_STATE = 0
 RUN_STATE = 1
@@ -23,7 +24,7 @@ class Monster:
         self.run_state_pos = [(10, 600, 70, 70), (90, 600, 70, 70), (170, 600, 70, 70), (250, 600, 70, 70), (330, 600, 70, 70), (415, 600, 75, 70), (330, 600, 70, 70), (250, 600, 70, 70)]
         self.attack_state_pos = [(10, 300, 70, 70), (85, 305, 70, 70), (160, 305, 70, 75), (240, 305, 70, 75), (300, 305, 70, 75), (380, 305, 70, 75), (475, 305, 70, 75), (565, 305, 70, 75)]
 
-        self.on_tile = None
+        self.on_tile = 8
 
     def update(self):
         if self.my_turn is False:
@@ -51,6 +52,7 @@ class Monster:
 
         if self.frame is 7:
             self.Change_to_IDLE()
+            self.on_tile -= 1
 
     def Attack_Animation(self):
         self.frame_x, self.frame_y, self.size_x, self.size_y = self.attack_state_pos[self.frame]
@@ -69,3 +71,11 @@ class Monster:
         else:
             self.my_turn = False
 
+    def My_Next_Action(self):
+        if self.my_turn is True:
+           next_action = random.randint(1, 3)
+
+           if self.on_tile is 2 or self.on_tile is 6 or self.on_tile is 10:
+               next_action = ATTACK_STATE
+
+           self.now_animation = next_action
